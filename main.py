5a2d5619741d63ced33051e76534a480b25c9775
kmro <k.mrozowski@versabox.pl>
2021-06-17 23:59:31 +0200

    experiment1-v3

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-
 from src.gen_algo import Population
-from test import Test
-
 
 if __name__ == "__main__":
+    old_population = None
+    crossover_mode = "mean"
+    selection_mode = "ranking"
+    generation_id = 0
+    pop_size = 9
+    n_workers = 8
+
     generation_count = 10000
 
-    population = Population(96, None,  crossover_mode="mean", selection_mode="ranking", generation_id=0)
+    population = Population(old_population,  crossover_mode, selection_mode, generation_id, pop_size, n_workers)
 
-    for generation in range(1, generation_count):
-        population = Population(96, population,  crossover_mode="mean", selection_mode="ranking", generation_id=generation)
-
-    # try:
-    #     #set_start_method('spawn')
-    #     score = Test(nn1, nn2, nn3)
-    #     print('score = ', score)
-    # except RuntimeError:
-    #     pass
+    for generation_id in range(1, generation_count):
+        population = Population(population, crossover_mode, selection_mode, generation_id, pop_size, n_workers)
--- src/gen_algo.py
+++ src/gen_algo.py
@@ -3,19 +3,14 @@ from typing import List
 
 import numpy as np
 import torch
-import torch.nn as nn
-import functools
 from torch.multiprocessing import Pool, Process, set_start_method
 
-
-from src.deep_q_network import DeepQNetwork
-from test_fit import one_thread_workout
-from test import Test
+from test_fit import one_thread_workout, crossover_prepare
 import pandas as pd
 
 elitism_pct = 0.2
 mutation_prob = 0.9
-weights_mutate_power = 0.04
+weights_mutate_power = 0.01
 mutation_decrement = 0.96
 device = 'cuda'
 
@@ -27,126 +22,123 @@ device = 'cuda'
 #4. Crossover
 #5. Mutation
 
+
+
 class Population:
-    def __init__(self, size=50, old_population=None, crossover_mode="mean", selection_mode="ranking", generation_id=0):
+    def __init__(
+            self,
+            old_population=None,
+            crossover_mode="mean",
+            selection_mode="ranking",
+            generation_id=0,
+            size=9,
+            n_workers=8):
         self.size = size
+        self.n_workers = n_workers
         self.generation_id = generation_id
         self.fitnesses = torch.zeros(self.size)
         if old_population is None:
             self.old_models = [torch.load("trained_models/tetris") for i in range(size)]
             self.models = [torch.load("trained_models/tetris") for i in range(size)]
             self.mutate()
-            self.multi_test(size)
+            self.evaluate()
             # self.pool_test(size)
             # self.fitnesses = np.array([Test(self.models[i], i) for i in range(size)])
-            self.succession()
             self.backup()
         else:
             #1. Population
             self.old_models = old_population.models
-            self.models = []
+            self.models = [torch.load("trained_models/tetris") for i in range(size)]
             self.old_fitnesses = old_population.fitnesses
 
-            self.crossover_mode = crossover_mode
-            self.selection_mode = selection_mode
-            self.crossover(crossover_mode, selection_mode)
+            self.sort_ids = np.zeros(self.size)
+            self.selected_ids = np.zeros(self.size)
+            self.selection(selection_mode)
+            self.crossover(crossover_mode)
             self.mutate()
-            self.multi_test(size)
+            self.evaluate()
             # self.pool_test(size)
             # self.fitnesses = np.array([Test(self.models[i], i) for i in range(size)])
             self.succession()
             self.backup()
 
-    def multi_test(self, size):
+    def evaluate(self):
+        print("Evaluating {} tetris neural-nets.......".format(self.size))
         set_start_method('spawn', force=True)
         processes: List[Process] = []
         self.fitnesses.share_memory_()
-        for i in range(6):
-            p = Process(target=one_thread_workout, args=(self.old_models, i, 16, self.fitnesses, ))
+        tests_in_queue = [np.floor_divide(self.size, self.n_workers) for _ in range(self.n_workers)]
+        for i in range(np.remainder(self.size, self.n_workers)):
+            tests_in_queue[i] += 1
+        for i in range(self.n_workers):
+            p = Process(target=one_thread_workout, args=(self.old_models, i, tests_in_queue, self.fitnesses, ))
             p.start()
             processes.append(p)
         for p in processes:
             p.join()
         pass
 
-    def pool_test(self, size):
+    def selection(self, selection_mode="ranking"):
+        print("Selection")
+        #selekcja rankingowa
+        if selection_mode == "ranking":
+            old_fitnesses = self.old_fitnesses.to().numpy()
+            sum_fitnesses = np.sum(np.power(old_fitnesses, 2))
+            probs = [np.power(old_fitnesses[i], 2) / sum_fitnesses for i in range(self.size)]
+
+            self.sort_ids = np.argsort(probs)[::-1]
+            print('\nall fitnesses: ', [old_fitnesses[i] for i in self.sort_ids])
+            pd.DataFrame([old_fitnesses[i] for i in self.sort_ids]).to_csv('best_models/fitness_history{}.csv'.format(self.generation_id))
+            best_model = self.old_models[self.sort_ids[0]]
+            print('best model fitness: {}'.format(self.old_fitnesses[self.sort_ids[0]]))
+            torch.save(best_model, "best_models/tetris_{}".format(self.generation_id))
+            print("Crossver: done:", end=" ")
+
+            for i in range(self.size):
+                rand = np.random.rand()
+                selected_id = -1
+                while rand > 0:
+                    selected_id += 1
+                    rand -= probs[selected_id]
+                self.selected_ids[i] = selected_id
+
+        self.selected_ids = self.selected_ids.astype(int)
+
+
+
+    def crossover(self, crossover_mode="mean"):
+        for i in range(3):
+            model_c = self.old_models[self.sort_ids[i]]
+            self.models[i] = model_c
+
         set_start_method('spawn', force=True)
-        with Pool(processes=2) as p:  # Paralleizing over 2 GPUs
-            results = p.apply_async(func=one_thread_workout, args=(self.old_models, 1, 2, self.fitnesses, ))
-            print(results)
+        processes: List[Process] = []
 
-    def crossover(self, crossover_mode="mean", selection_mode="ranking"):
-        #2. Fitness
-        old_fitnesses = self.old_fitnesses.to().numpy()
-        sum_fitnesses = np.sum(old_fitnesses)
-        probs = [old_fitnesses[i] / sum_fitnesses for i in
-                 range(self.size)]
+        crossovers_in_queue = [np.floor_divide(self.size, self.n_workers) for _ in range(self.n_workers)]
+        for i in range(np.remainder(self.size, self.n_workers)):
+            crossovers_in_queue[i] += 1
+        for i in range(3, self.n_workers):
+            p = Process(target=crossover_prepare, args=(crossovers_in_queue, self.size, self.selected_ids,
+                        self.old_models, crossover_mode, i, self.old_models, ))
+            processes.append(p)
+        for p in processes:
+            p.start()
+        for p in processes:
+            p.join()
+        pass
 
-        # Sorting descending NNs according to their fitnesses
-        #3. Parents selection
-        sort_indices = np.argsort(probs)[::-1]
-        print('all fitnesses: ', [old_fitnesses[i] for i in sort_indices])
-        pd.DataFrame([old_fitnesses[i] for i in sort_indices]).to_csv('best_models/fitness_history{}.csv'.format(self.generation_id))
-        best_model = self.old_models[sort_indices[0]]
-        print('best model fitness: {}'.format(self.old_fitnesses[sort_indices[0]]))
-        torch.save(best_model, "best_models/tetris_{}".format(self.generation_id))
-        print("Crossver: done:", end=" ")
-
-        for i in range(self.size):
-            if i < 3: #self.size * elitism_pct:
-                # Add the top performing childs - parents selection
-                model_c = self.old_models[sort_indices[i]]
-                self.models.append(model_c)
-            else:
-                #selekcja rankingowa
-                if selection_mode == "ranking":
-                    a = sort_indices[0]
-                    b = sort_indices[1]
-                # sum_parent = self.old_fitnesses[a] + self.old_fitnesses[b]
-
-                model_a, model_b = self.old_models[a], self.old_models[b]
-                model_c = torch.load("trained_models/tetris")
-
-                conv_a = [model_a.conv1, model_a.conv2, model_a.conv3]
-                conv_b = [model_b.conv1, model_b.conv2, model_b.conv3]
-                conv_c = [model_c.conv1, model_c.conv2, model_c.conv3]
-
-                for c_i in range(len(conv_b)):
-                    #4. Crossover
-                    for conv in range(3):
-                        for i in range(conv_c[c_i][0].weight.size()[0]):
-                            for j in range(conv_c[c_i][0].weight.size()[1]):
-                                if crossover_mode == "mean":
-                                    a = np.random.random()
-                                    conv_c[c_i][0].weight.data[i][j] = a*conv_b[c_i][0].weight.data[i][j] + (1-a)*conv_a[c_i][0].weight.data[i][j]
-                                if crossover_mode == "two_point":
-                                    point_one = np.random.random()
-                                    point_two = np.random.random()
-
-                                    if point_one > point_two:
-                                        a = point_one
-                                        point_one = point_two
-                                        point_two = a
-
-                                    conv_c[c_i][0].weight.data[0:point_one][j] = conv_b[c_i][0].weight.data[0:point_one][j]
-                                    conv_c[c_i][0].weight.data[point_one:point_two][j] = conv_c[c_i][0].weight.data[point_one:point_two][j]
-                                    conv_c[c_i][0].weight.data[point_two:][j] = conv_b[c_i][0].weight.data[point_two:][j]
-                self.models.append(model_c)
-            print(i, end=" ")
-
-    #5. Mutate
     def mutate(self):
         mutate_power = weights_mutate_power * mutation_decrement ** self.generation_id
         print("\nMutating, Power={}, Finished: ".format(mutate_power), end=" ")
         for i in range(3, self.size):
-            convs = [self.models[i].conv1, self.models[i].conv2, self.models[i].conv3]
-            for conv in convs:
+            print(i, end=" ")
+            for conv in [self.models[i].conv1, self.models[i].conv2, self.models[i].conv3]:
                 # for i in range(conv[0].weight.size()[0]):
                 if np.random.random() < mutation_prob:
                     noise = torch.randn(1).mul_(mutate_power).to(device)
                     #add noise to each of neuron
                     conv[0].weight.data.add_(noise[0])
-            print(i, end=" ")
+        print("")
 
     def succession(self):
         print("\nSuccession: worse ids:", end=" ")
