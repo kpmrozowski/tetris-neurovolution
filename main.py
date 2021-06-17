a6a071f23b68e96045b5aa55a4b0f6af0b7750c8
Mrozowski, Kornel <kpmrozowski@gmail.com>
2021-06-17 08:52:31 +0200

    experiment-1

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ from test import Test
 
 
 if __name__ == "__main__":
-    generation_count = 3
+    generation_count = 10000
 
-    population = Population(4, None,  crossover_mode="mean", selection_mode="ranking", generation_id=0)
+    population = Population(96, None,  crossover_mode="mean", selection_mode="ranking", generation_id=0)
 
     for generation in range(1, generation_count):
-        population = Population(4, population,  crossover_mode="mean", selection_mode="ranking", generation_id=generation)
+        population = Population(96, population,  crossover_mode="mean", selection_mode="ranking", generation_id=generation)
 
     # try:
     #     #set_start_method('spawn')
--- src/gen_algo.py
+++ src/gen_algo.py
@@ -14,9 +14,9 @@ from test import Test
 import pandas as pd
 
 elitism_pct = 0.2
-mutation_prob = 0.2
-weights_mutate_power = 0.01
-
+mutation_prob = 0.9
+weights_mutate_power = 0.05
+mutation_decrement = 0.96
 device = 'cuda'
 
 #Genetic algorithm
@@ -57,8 +57,8 @@ class Population:
         set_start_method('spawn', force=True)
         processes: List[Process] = []
         self.fitnesses.share_memory_()
-        for i in range(2):
-            p = Process(target=one_thread_workout, args=(self.old_models, i, 2, self.fitnesses, ))
+        for i in range(6):
+            p = Process(target=one_thread_workout, args=(self.old_models, i, 16, self.fitnesses, ))
             p.start()
             processes.append(p)
         for p in processes:
@@ -87,7 +87,7 @@ class Population:
         pd.DataFrame(self.old_fitnesses).to_csv('best_models/fitness_history{}.csv'.format(self.generation_id))
 
         for i in range(self.size):
-            if i < self.size * elitism_pct:
+            if i < 3: #self.size * elitism_pct:
                 # Add the top performing childs - parents selection
                 model_c = self.old_models[sort_indices[i]]
                 self.models.append(model_c)
@@ -126,19 +126,20 @@ class Population:
                                     conv_c[c_i][0].weight.data[point_one:point_two][j] = conv_c[c_i][0].weight.data[point_one:point_two][j]
                                     conv_c[c_i][0].weight.data[point_two:][j] = conv_b[c_i][0].weight.data[point_two:][j]
                 self.models.append(model_c)
-            print(i, '-done')
+            # print(i, '-done')
 
     #5. Mutate
     def mutate(self):
-        print("Mutating")
-        for i in range(self.size):
+        mutate_power = weights_mutate_power * mutation_decrement ** self.generation_id
+        print("Mutating, Power =", mutate_power)
+        for i in range(3, self.size):
             convs = [self.models[i].conv1, self.models[i].conv2, self.models[i].conv3]
             for conv in convs:
                 # for i in range(conv[0].weight.size()[0]):
                 if np.random.random() < mutation_prob:
-                    noise = torch.randn(1).mul_(weights_mutate_power).to(device)
+                    noise = torch.randn(1).mul_(mutate_power).to(device)
                     #add noise to each of neuron
                     conv[0].weight.data.add_(noise[0])
-            print(i, '-done')
+            # print(i, '-done')
 
 
